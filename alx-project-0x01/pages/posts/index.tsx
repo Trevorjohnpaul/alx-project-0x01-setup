cf6c8d7a58777a4c98c415892793350174c59d8b
Trevor Johnpaul <trevorjohnpaul4@gmail.com>
2025-11-29 13:08:12 +0300

    Navigation betweern pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,7 +1,14 @@
+import Header from "@/components/layout/Header";
+import React from "react";
+
 const Posts: React.FC = () => {
   return (
-    <div className="p-4">
-      <h1>Posts Page</h1>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="p-4">
+        <h1 className="text-2xl font-semibold">Posts Page</h1>
+        {/* Add your post content here */}
+      </main>
     </div>
   );
 };
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,7 +1,14 @@
+import Header from "@/components/layout/Header";
+import React from "react";
+
 const Users: React.FC = () => {
   return (
-    <div className="p-4">
-      <h1>Users Page</h1>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="p-4">
+        <h1 className="text-2xl font-semibold">Users Page</h1>
+        {/* Add your users content here */}
+      </main>
     </div>
   );
 };
