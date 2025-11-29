35fc5c47c79e40110892d9ba78ccff8420b191cd
Trevor Johnpaul <trevorjohnpaul4@gmail.com>
2025-11-29 13:19:37 +0300

    Correction

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -2,19 +2,19 @@ import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
 import { PostProps } from "@/interfaces";
 
+// Posts component
 const Posts: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
-  console.log(posts);
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
         <div className="flex justify-between">
-          <h1 className=" text-2xl font-semibold">Post Content</h1>
+          <h1 className="text-2xl font-semibold">Post Content</h1>
           <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
             Add Post
           </button>
         </div>
-        <div className="grid grid-cols-3 gap-2 ">
+        <div className="grid grid-cols-3 gap-2">
           {posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
             <PostCard
               title={title}
